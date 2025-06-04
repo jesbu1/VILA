d021f2375173aca304684b6cbe15c0d8977fac6c
Jesse Zhang <jessezhang5@gmail.com>
2025-06-03 23:38:20 -0700

    update vila 3b server path_mask name

--- vila_3b_server.py
+++ vila_3b_server.py
@@ -35,8 +35,8 @@ from server import ChatMessage, TextContent, ImageURL, ImageContent, get_literal
 
 IMAGE_CONTENT_BASE64_REGEX = re.compile(r"^data:image/(png|jpe?g);base64,(.*)$")
 PATH_MODEL_NAME = "vila_3b_oxe_sim_path"
-PATH_MASK_MODEL_NAME = "vila_3b_oxe_sim_path_mask"
-EVERYTHING_MODEL_NAME = "vila_3b_oxe_sim_jack_o"
+PATH_MASK_MODEL_NAME = "vila_3b_path_mask"
+# EVERYTHING_MODEL_NAME = "vila_3b_oxe_sim_jack_o"
 
 
 
