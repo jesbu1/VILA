bdf950199c40685331209acb45b9a8e526ee31dc
Jesse Zhang <jessezhang5@gmail.com>
2025-06-14 17:37:59 -0700

    update camera name to statioanry

--- label_hackathon_data.py
+++ label_hackathon_data.py
@@ -39,7 +39,7 @@ CUDA_VISIBLE_DEVICES=0 python label_hackathon_data.py \
     --args.batch-size=16
 """
 
-CAM_NAME = "336222072709"
+CAM_NAME = "stationary"
 
 import logging
 from pathlib import Path
