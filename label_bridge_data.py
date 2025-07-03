bb2e58c60b0fa17770ee07d56be213493917dc57
Jesse Zhang <jessezhang5@gmail.com>
2025-07-03 15:37:01 -0700

    update default commands

--- label_bridge_data.py
+++ label_bridge_data.py
@@ -32,10 +32,12 @@ pip install shapely
 pip install tensorflow
 
 
+    #--args.model-path ~/.cache/huggingface/hub/models--memmelma--vila_13b_path_mask_new/snapshots/08855b9bda093a96fe452bb9fa300564f4760e4a/checkpoint-11500/  \
+
 CUDA_VISIBLE_DEVICES=0 python label_bridge_data.py \
     --args.data-dir=/data/shared/openx_rlds_data/ \
-    --args.output-dir=./test_bridge_labeling_13b \
-    --args.model-path ~/.cache/huggingface/hub/models--memmelma--vila_13b_path_mask_new/snapshots/08855b9bda093a96fe452bb9fa300564f4760e4a/checkpoint-11500/  \
+    --args.output-dir=./test_bridge_labeling_3b \
+    --args.model-path ~/.cache/huggingface/hub/models--memmelma--vila_3b_path_mask_fast/snapshots/12df7a04221a50e88733cd2f1132eb01257aba0d/checkpoint-11700/ \
     --args.batch-size=4 \
     --args.vlm-call-frequency=60 \
     --args.load-8bit \
@@ -44,8 +46,8 @@ CUDA_VISIBLE_DEVICES=0 python label_bridge_data.py \
 
 CUDA_VISIBLE_DEVICES=1 python label_bridge_data.py \
     --args.data-dir=/data/shared/openx_rlds_data/ \
-    --args.output-dir=./test_bridge_labeling_13b \
-    --args.model-path ~/.cache/huggingface/hub/models--memmelma--vila_13b_path_mask_new/snapshots/08855b9bda093a96fe452bb9fa300564f4760e4a/checkpoint-11500/  \
+    --args.output-dir=./test_bridge_labeling_3b \
+    --args.model-path ~/.cache/huggingface/hub/models--memmelma--vila_3b_path_mask_fast/snapshots/12df7a04221a50e88733cd2f1132eb01257aba0d/checkpoint-11700/  \
     --args.vlm-call-frequency=60 \
     --args.load-8bit \
     --args.batch-size=4 \
