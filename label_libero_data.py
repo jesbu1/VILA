4e3f7be879a6e029e4636003addb77936059ead7
Jesse Zhang <jessez@usc.edu>
2025-07-24 16:54:08 -0700

    get rid fo the flip lr

--- label_libero_data.py
+++ label_libero_data.py
@@ -162,6 +162,15 @@ def generate_paths_masks(args: Args) -> None:
 
                 # Get paths and masks using direct VLM inference
                 try:
+                    for i in range(len(episode_images)):
+                        if "right" in episode_tasks[i]:
+                            episode_tasks[i] = episode_tasks[i].replace(
+                                "right", "left"
+                            )
+                        elif "left" in episode_tasks[i]:
+                            episode_tasks[i] = episode_tasks[i].replace(
+                                "left", "right"
+                            )
                     #if episode_idx == 10:   
                     #for i in range(len(episode_tasks)):
                     #    if "right" in episode_tasks[i]:
@@ -169,7 +178,7 @@ def generate_paths_masks(args: Args) -> None:
                     #    elif "left" in episode_tasks[i]:
                     #        episode_tasks[i] = episode_tasks[i].replace("left", "right")
                     # flip the images horizontally
-                    episode_images = [np.fliplr(img) for img in episode_images]
+                    #episode_images = [np.fliplr(img) for img in episode_images]
                     # for LIBERO we can just iterate over each episode image for the path history thing
 
                     # for history
@@ -194,6 +203,7 @@ def generate_paths_masks(args: Args) -> None:
                     #    all_masks.extend(masks)
                     #paths = all_paths
                     #masks = all_masks
+
                     paths, masks = get_path_mask_from_vlm_direct(
                         episode_images,
                         episode_tasks,
