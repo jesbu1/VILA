88f882c881c4875c919dc1a8a88976deb7a113e5
Jesse Zhang <jessezhang5@gmail.com>
2025-07-28 14:24:39 -0700

    fix fractal data left-right swapping

--- label_fractal_data.py
+++ label_fractal_data.py
@@ -166,20 +166,6 @@ def generate_paths_masks(args: Args) -> None:
 
                 # Get paths and masks using direct VLM inference
                 try:
-                    for i in range(len(episode_images)):
-                        if "right" in episode_tasks[i]:
-                            episode_tasks[i] = episode_tasks[i].replace("right", "left")
-                        elif "left" in episode_tasks[i]:
-                            episode_tasks[i] = episode_tasks[i].replace("left", "right")
-                    # if episode_idx == 10:
-                    # for i in range(len(episode_tasks)):
-                    #    if "right" in episode_tasks[i]:
-                    #        episode_tasks[i] = episode_tasks[i].replace("right", "left")
-                    #    elif "left" in episode_tasks[i]:
-                    #        episode_tasks[i] = episode_tasks[i].replace("left", "right")
-                    # flip the images horizontally
-                    # episode_images = [np.fliplr(img) for img in episode_images]
-                    # for LIBERO we can just iterate over each episode image for the path history thing
 
                     # for history
                     # all_paths = []
