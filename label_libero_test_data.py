b45daa72ee0eef3244bca3ce091373e4f1a86f00
Jesse Zhang <jessez@usc.edu>
2025-07-31 11:45:27 -0700

    flip horizontally

--- label_libero_test_data.py
+++ label_libero_test_data.py
@@ -209,17 +209,17 @@ def generate_paths_masks(args: Args) -> None:
 
                         # Get paths and masks using direct VLM inference
                         try:
-                            for i in range(len(episode_tasks)):
-                                if "right" in episode_tasks[i]:
-                                    episode_tasks[i] = episode_tasks[i].replace(
-                                        "right", "left"
-                                    )
-                                elif "left" in episode_tasks[i]:
-                                    episode_tasks[i] = episode_tasks[i].replace(
-                                        "left", "right"
-                                    )
+                            #for i in range(len(episode_tasks)):
+                            #    if "right" in episode_tasks[i]:
+                            #        episode_tasks[i] = episode_tasks[i].replace(
+                            #            "right", "left"
+                            #        )
+                            #    elif "left" in episode_tasks[i]:
+                            #        episode_tasks[i] = episode_tasks[i].replace(
+                            #            "left", "right"
+                            #        )
                             # flip the images horizontally
-                            # episode_images = [np.fliplr(img) for img in episode_images]
+                            episode_images = [np.fliplr(img) for img in episode_images]
                             # for LIBERO we can just iterate over each episode image for the path history thing
 
                             # for history
